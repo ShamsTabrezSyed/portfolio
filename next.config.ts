dd62b5e72432382f8fd43b73347c4a8b8e813ad0
ShamsTabrezSyed <shams.syed0525@gmail.com>
2026-04-28 22:20:38 -0500

    Fix build for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/portfolio',
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
